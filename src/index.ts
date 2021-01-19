a35fde8ecc9af3f849a4e0ecf087983c385115cf
yuankun <yuankun.yk@alibaba-inc.com>
2021-01-19 23:02:22 +0800

    feat: s组件打包构建能力

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { Component } from '@serverless-devs/s-core';
+
 export default () => {
+  // eslint-disable-next-line no-new
+  new Component('123');
   const a = true;
-  console.log(a);
+  console.log(a / 0);
   return a;
 };
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.json',
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+  },
+};
