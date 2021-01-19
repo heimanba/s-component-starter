ddfbbca3e0fe3a1ec8ea708e14c8a18b08a8f1e4
yuankun <yuankun.yk@alibaba-inc.com>
2021-01-19 23:33:20 +0800

    feat: 增加s组件库demo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Component } from '@serverless-devs/s-core';
 
-export default () => {
-  // eslint-disable-next-line no-new
-  new Component('123');
-  const a = true;
-  console.log(a / 0);
-  return a;
-};
+export default class MyComponent extends Component {
+  async test(inputs) {
+    // 返回结果
+    return {
+      Result: 'hello world',
+      Args: inputs,
+    };
+  }
+}
