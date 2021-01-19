a067380580efcac5b6b5ac68d279a61f19811713
yuankun <yuankun.yk@alibaba-inc.com>
2021-01-19 15:42:25 +0800

    feat: init 初始化

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export default () => {
+  const a = true;
+  console.log(a);
+  return a;
+};
---
+++ test/index.test.ts
@@ -0,0 +1,5 @@
+describe('test/abc.test.ts', () => {
+  it('should abc.test.ts', () => {
+    expect('123').toBe('123');
+  });
+});
