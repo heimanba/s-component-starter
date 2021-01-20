9c5f1f42515705898493d55237f5cec99ef6068d
yuankun <yuankun.yk@alibaba-inc.com>
2021-01-20 10:59:49 +0800

    feat: 制作成标准组件形式

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Component } from '@serverless-devs/s-core';
 
-export default class MyComponent extends Component {
+export default class ComponentStarter extends Component {
   async test(inputs) {
     // 返回结果
     return {
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,18 @@
-describe('test/abc.test.ts', () => {
-  it('should abc.test.ts', () => {
-    expect('123').toBe('123');
+import ComponentStarter from '../src/index';
+
+describe('test/index.test.ts', () => {
+  it('should 返回输入参数', async () => {
+    const componentStarter = new ComponentStarter();
+    const argParams = {
+      Region: 'cn-hangzhou',
+      CodeUri: './src',
+      ProjectName: 'ExpressComponent',
+      Component: 'component-stater',
+      Provider: 'alibaba',
+      AccessAlias: '',
+      Command: 'test',
+    };
+    const result = await componentStarter.test(argParams);
+    expect(result.Result).toBe('hello world');
   });
 });
